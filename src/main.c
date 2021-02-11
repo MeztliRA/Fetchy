67b9868e3a0ab9221b9fd57bddb7cb86c6e73636
MeztliRA <MeztliRA@gmail.com>
2021-02-11 16:36:51 +0700

    Add os to the info shown

--- src/main.c
+++ src/main.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/utsname.h>
 
 int main() {
+    struct utsname uname_data;
+
     char * desktop = getenv("XDG_CURRENT_DESKTOP");
     char * user = getenv("USER");
     char * shell = getenv("SHELL");
@@ -10,8 +13,11 @@ int main() {
     char hostname[1024];
     gethostname(hostname, 1024);
 
+    uname(&uname_data);
+
     printf("hostname: %s\n", hostname);
     printf("user: %s\n", user);
+    printf("os: %s %s\n", uname_data.sysname, uname_data.release);
     printf("shell: %s\n", shell);
     printf("desktop: %s\n", desktop);
     printf("language: %s\n", language);

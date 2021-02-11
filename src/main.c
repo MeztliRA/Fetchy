4a313f3c811539ffa8b85c8e9ff0bc1126e38f32
MeztliRA <MeztliRA@gmail.com>
2021-02-11 15:55:02 +0700

    Added hostname to info shown

--- src/main.c
+++ src/main.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 int main() {
     char * desktop = getenv("XDG_CURRENT_DESKTOP");
     char * user = getenv("USER");
     char * shell = getenv("SHELL");
     char * language = getenv("LANG");
+    char hostname[1024];
+    gethostname(hostname, 1024);
 
+    printf("hostname: %s\n", hostname);
     printf("user: %s\n", user);
     printf("shell: %s\n", shell);
     printf("desktop: %s\n", desktop);
